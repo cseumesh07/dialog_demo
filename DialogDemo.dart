abd2bb2c2989783f216548d24fa1b7d6f9b6819b
umeshlaxmi07 <klau.tiwari1234@gmail.com>
2021-09-25 00:35:00 +0530

    initial commit

---
+++ DialogDemo.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(MyStatelessApp());
+}
+
+class MyStatelessApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dialog Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: StatelessWidgetDemo(),
+    );
+  }
+}
+
+class StatelessWidgetDemo extends StatefulWidget {
+  @override
+  _StatelessWidgetDemoState createState() => _StatelessWidgetDemoState();
+}
+
+class _StatelessWidgetDemoState extends State<StatelessWidgetDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text('Dialog Demo'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      _showMaterialDialog();
+                    },
+                    child: Text('Show Material Dialog'),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _showCupertinoDialog();
+                    },
+                    child: Text('Show Cupertino Dialog'),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      _showSimpleDialog();
+                    },
+                    child: Text('Show Simple Dialog'),
+                  )
+                ],
+              ),
+            )));
+  }
+
+  void _showMaterialDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Material Dialog'),
+            content: Text('This Is My Material Dailog And This Is How I look!'),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    _dismissDialog();
+                  },
+                  child: Text('Close')),
+              TextButton(
+                onPressed: () {
+                  print('HelloWorld!');
+                  _dismissDialog();
+                },
+                child: Text('HelloWorld!'),
+              )
+            ],
+          );
+        });
+  }
+
+  _dismissDialog() {
+    Navigator.pop(context);
+  }
+
+  void _showCupertinoDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text('Cupertino Dialog'),
+            content: Text('Hey! I am Coflutter!'),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    _dismissDialog();
+                  },
+                  child: Text('Close')),
+              TextButton(
+                onPressed: () {
+                  print('HelloWorld!');
+                  _dismissDialog();
+                },
+                child: Text('HelloWorld!'),
+              )
+            ],
+          );
+        });
+  }
+
+  void _showSimpleDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return SimpleDialog(
+            title: Text('Chosse an Option'),
+            children: <Widget>[
+              SimpleDialogOption(
+                onPressed: () {
+                  _dismissDialog();
+                },
+                child: const Text('Option 1'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  _dismissDialog();
+                },
+                child: const Text('Option 2'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  _dismissDialog();
+                },
+                child: const Text('Option 3'),
+              ),
+              SimpleDialogOption(
+                onPressed: () {
+                  _dismissDialog();
+                },
+                child: const Text('Option 4'),
+              ),
+            ],
+          );
+        });
+  }
+}
